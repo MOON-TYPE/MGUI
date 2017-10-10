853375ca97b59a29a8091dbfa5031d4385d2c6e3
Moon Antonio <pinchodev91@gmail.com>
2017-10-11 00:59:15 +0200

    Constructor UIPanel

--- MGUI Project/Assets/Moon Antonio/MGUI/Scripts/Panel/UIPanel.cs
+++ MGUI Project/Assets/Moon Antonio/MGUI/Scripts/Panel/UIPanel.cs
@@ -2,7 +2,7 @@
 //																				\\
 // UIPanel.cs (11/10/2017)														\\
 // Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
-// Descripcion:																	\\
+// Descripcion:		Control del panel de la UI									\\
 // Fecha Mod:		11/10/2017													\\
 // Ultima Mod:		Version Inicial												\\
 //******************************************************************************\\
@@ -23,6 +23,27 @@ namespace MoonAntonio.MGUI
 	[DisallowMultipleComponent, ExecuteInEditMode, AddComponentMenu("MGUI/Panel", 58), RequireComponent(typeof(CanvasGroup))]
 	public class UIPanel : MonoBehaviour, IEventSystemHandler, ISelectHandler, IPointerDownHandler
 	{
+		#region Variables Privadas
+		/// <summary>
+		/// <para>Control del tween.</para>
+		/// </summary>
+		[NonSerialized] private readonly TweenRunner<FloatTween> tweenRun;					// Control del tween
+		#endregion
+
+		#region Constructor Interno
+		/// <summary>
+		/// <para>[Unity] Llamada por Unity antes de la deserializacion, no debe ser llamado.</para>
+		/// </summary>
+		protected UIPanel()
+		{
+			// Si no hay tween, inicializar uno.
+			if (this.tweenRun == null) this.tweenRun = new TweenRunner<FloatTween>();
+
+			// Iniciar el tween
+			this.tweenRun.Init(this);
+		}
+		#endregion
+
 		#region Enums
 		/// <summary>
 		/// <para>Transicion del panel.</para>
