c819de54f446349df2bf3fb92909feee870ab2cc
Moon Antonio <pinchodev91@gmail.com>
2017-10-11 20:36:41 +0200

    Metodo Awake UIDrag

--- MGUI Project/Assets/Moon Antonio/MGUI/Scripts/Componentes/UIDrag.cs
+++ MGUI Project/Assets/Moon Antonio/MGUI/Scripts/Componentes/UIDrag.cs
@@ -23,6 +23,42 @@ namespace MoonAntonio.MGUI
 	[AddComponentMenu("MGUI/UIDrag", 82)]
 	public class UIDrag : UIBehaviour
 	{
+		#region Variables Publicas
+		/// <summary>
+		/// <para>Objetivo del arrastre.</para>
+		/// </summary>
+		private RectTransform target;												// Objetivo del arrastre
+		#endregion
 
+		#region Variables Privadas
+		/// <summary>
+		/// <para>Canvas de la escena.</para>
+		/// </summary>
+		private Canvas canvas;														// Canvas de la escena
+		/// <summary>
+		/// <para>Canvas del Rect Transform.</para>
+		/// </summary>
+		private RectTransform canvasRectTransform;									// Canvas del Rect Transform
+		#endregion
+
+		#region Clases Eventos
+		[Serializable] public class BeginDragEvent : UnityEvent<BaseEventData> { }
+		[Serializable] public class EndDragEvent : UnityEvent<BaseEventData> { }
+		[Serializable] public class DragEvent : UnityEvent<BaseEventData> { }
+		#endregion
+
+		#region Inicializadores
+		/// <summary>
+		/// <para>Cargador de <see cref="UIDrag"/>.</para>
+		/// </summary>
+		protected override void Awake()// Cargador de UIDrag
+		{
+			base.Awake();
+
+			// Obtener el canvas y el rect transform
+			this.canvas = UIUtil.FindInParents<Canvas>((this.target != null) ? this.target.gameObject : this.gameObject);
+			if (this.canvas != null) this.canvasRectTransform = this.canvas.transform as RectTransform;
+		}
+		#endregion
 	}
 }
