3eaf2fbb523f383d1b771e3af73e1e37aa9111b6
Moon Antonio <pinchodev91@gmail.com>
2017-10-11 20:24:34 +0200

    Init UIDrag

--- MGUI Project/Assets/Moon Antonio/MGUI/Scripts/Componentes/UIDrag.cs
+++ MGUI Project/Assets/Moon Antonio/MGUI/Scripts/Componentes/UIDrag.cs
@@ -1,19 +1,27 @@
 ﻿//                                  ┌∩┐(◣_◢)┌∩┐
 //																				\\
-// UIDrag.cs (00/00/0000)													\\
-// Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com								        \\
-// Descripcion:																	\\
-// Fecha Mod:		00/00/0000													\\
-// Ultima Mod:																	\\
+// UIDrag.cs (11/10/2017)														\\
+// Autor: Antonio Mateo (Moon Antonio) 	antoniomt.moon@gmail.com				\\
+// Descripcion:		Control para arrastras un objeto							\\
+// Fecha Mod:		11/10/2017													\\
+// Ultima Mod:		Version Inicial												\\
 //******************************************************************************\\
 
 #region Librerias
+using System;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 #endregion
 
-namespace MoonAntonio
+namespace MoonAntonio.MGUI
 {
-	public class UIDrag : MonoBehaviour 
+	/// <summary>
+	/// <para>Control para arrastras un objeto</para>
+	/// </summary>
+	[AddComponentMenu("MGUI/UIDrag", 82)]
+	public class UIDrag : UIBehaviour
 	{
 
 	}
